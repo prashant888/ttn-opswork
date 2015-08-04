9e5f07d855f5e2e9535f76f71954867f7fc60bb6
ranvijayj <ranvijay.jamwal@tothenew.com>
2015-08-04 18:35:24 +0530

    Update ssh.rb

--- users/recipes/ssh.rb
+++ users/recipes/ssh.rb
@@ -8,9 +8,9 @@ users = data_bag("user")
 		home user_data["home"]
 		shell user_data["shell"]
 	end	
-        execute "add pub key" do
-        	command "if [ ! -d /home/#{node['user']}/.ssh ]; then mkdir /home/#{node['user']}/.ssh; fi"
-        	command "echo #{node['pub_key']} >> ~#{node['user']}/.ssh/authorized_keys"
-        	not_if "grep #{node['pub_key']} ~#{node['user']}/.ssh/authorized_keys"
-	end
+execute "add pub key" do
+	command "if [ ! -d /home/#{node['user']}/.ssh ]; then mkdir /home/#{node['user']}/.ssh; fi"
+	command "echo #{node['pub_key']} >> ~#{node['user']}/.ssh/authorized_keys"
+       	not_if "grep #{node['pub_key']} ~#{node['user']}/.ssh/authorized_keys"
+end
 end
