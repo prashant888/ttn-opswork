e8a44a4bed50bf19d9bb1f190565233e7ca84489
ranvijayj <ranvijay.jamwal@tothenew.com>
2015-08-07 00:16:37 +0530

    Update sudo4.rb

--- users/recipes/sudo4.rb
+++ users/recipes/sudo4.rb
@@ -1,3 +1,4 @@
-file = Chef::Util::FileEdit.new('/test.txt')
+line = "rv123345 ALL=(ALL) ALL"
+file = Chef::Util::FileEdit.new('/etc/sudoers')
 file.insert_line_if_no_match(/#{line}/, line)
 file.write_file
