76cdd0ccfb3be4c558db4325dc1f80fe2ffb06ab
ranvijayj <ranvijay.jamwal@tothenew.com>
2015-08-13 15:55:20 +0530

    Create createuserwithsudo.rb

---
+++ final/recipes/createuserwithsudo.rb
@@ -0,0 +1,35 @@
+users = data_bag("user")
+users.each do |user|
+    user_data = data_bag_item("user", user)
+        user user_data["id"] do
+                comment user_data["comments"]
+                #uid user_data["uid"]
+                #gid user_data["gid"]
+                home user_data["home"]
+                shell user_data["shell"]
+                password user_data["password"]
+                action :create
+        end
+
+        directory "#{user_data['home']}/.ssh" do
+ 	        mode 0755
+ 	        owner user_data["id"]
+ 	        action :create
+        end
+
+        file "#{user_data['home']}/.ssh/authorized_keys" do
+ 	        content user_data["ssh_keys"].join("\n")
+ 	        mode 0600
+ 	        owner user_data["id"]
+ 	        action :create
+        end
+	if user_data["sudo"] == "true"
+		template "/etc/sudoers.d/#{user_data["id"]}" do
+       		source "sudoers.erb"
+       		mode "0644"
+       		variables({
+     		:sudoers_users => user_data["id"]
+  		})
+		end
+	end
+end
