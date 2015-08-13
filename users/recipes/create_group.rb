6a398dddc83edc278e1f086d6b8a656fea94a834
ranvijayj <ranvijay.jamwal@tothenew.com>
2015-08-13 19:34:48 +0530

    Update create_group.rb

--- users/recipes/create_group.rb
+++ users/recipes/create_group.rb
@@ -1,4 +1,15 @@
-group "sap" do
-  action :create
-  members "matt"
+groups = data_bag("group")
+groups.each do |group|
+        group_data = data_bag_item("group", group)
+        groups group_data["gid"] do
+             gid group_data["gid"]
+		         members group_data["members"] 
+        end
 end
+#groups = data_bag_item("group")
+#search("groups","*:*").each do |group_data|
+#	group group_data["id"] do
+		
+#	end
+#end
+#
