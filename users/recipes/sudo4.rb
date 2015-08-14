a2a631cc9f9cfc8257ef17d95252d0c1520a634a
ranvijayj <ranvijay.jamwal@tothenew.com>
2015-08-14 14:44:18 +0530

    Update sudo4.rb

--- users/recipes/sudo4.rb
+++ users/recipes/sudo4.rb
@@ -1,4 +1,11 @@
-line = "rv123345	ALL=(ALL) ALL"
-file = Chef::Util::FileEdit.new('/etc/sudoers')
+line = "%sap    ALL= NOPASSWD: /usr/sbin/apache2*
+%sap    ALL= NOPASSWD: /usr/local/bin/apache2*
+#%sap    ALL= NOPASSWD: /usr/local/bin/restart-sauce-connect
+# Temp, so Brian can't accidentally start something we can't stop.
+%sap    ALL= NOPASSWD: /usr/local/bin/restart-sauce-connect
+jenkins ALL= NOPASSWD: /usr/sbin/apache2*
+jenkins ALL= NOPASSWD: /usr/local/bin/restart-sauce-connect
+"
+file = Chef::Util::FileEdit.new('/etc/sudoers.d/adoptapet')
 file.insert_line_if_no_match(/#{line}/, line)
 file.write_file
