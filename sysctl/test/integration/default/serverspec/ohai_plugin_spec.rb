a57608518965505e790a789fa94cde142ef3fd23
Prashant <prashant.aws@gmail.com>
2016-02-18 16:00:50 +0530

    mysql-9

--- sysctl/test/integration/default/serverspec/ohai_plugin_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe command('ohai -d /etc/chef/ohai_plugins sys') do
-  it { should return_exit_status 0 }
-  it { should return_stdout(/"net": {/) }
-end
--- sysctl/test/integration/default/serverspec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'serverspec'
-
-include Serverspec::Helper::Exec
-
-def os
-  backend(Serverspec::Commands::Base).check_os
-end
