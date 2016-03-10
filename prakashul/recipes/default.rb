5fe4e0e9f6ee5e1f393b755fe57f7da0309ee8ae
prakashul <prakashul@tothenew.com>
2016-03-10 22:40:12 +0530

    enable vhost

--- prakashul/recipes/default.rb
+++ prakashul/recipes/default.rb
@@ -14,12 +14,18 @@ package 'tomcat7' do
         action :install
 end
 
-file "/etc/nginx/sites-available/default-prakashul" do
+file "/etc/nginx/sites-available/default" do
+ action :delete
+end
+
+file "/etc/nginx/sites-available/default" do
   owner 'root'
   group 'root'
   mode 0755
   content ::File.open("/var/chef/cookbooks/prakashul/files/default/default1").read
   action :create
+  notifies :reload, "service[nginx]"
+  notifies :restart, "service[tomcat7]"
 end
   	
 service 'tomcat7' do
