23231c38cf24984996499497bc9ea30037bbd44e
prakashul <prakashul@tothenew.com>
2016-03-11 13:46:27 +0530

    nagios server changes

---
+++ nagios/recipes/hosts.rb
@@ -0,0 +1,20 @@
+
+all_hosts = {}
+all_hostgroups = {}
+
+node['opsworks']['layers'].each do |layer_id, layer_info|
+  all_hostgroups[layer_id] = layer_info['name']
+  layer_info['instances'].each do |instance_name, instance_info|
+    if instance_info['status'] == 'online'
+      node_hostgroups[instance_info['availability_zone']] = instance_info['availability_zone']
+      node_hostgroups[layer_id] = layer_info['name']
+
+      all_hostgroups = all_hostgroups.merge(node_hostgroups)
+      if all_hosts.has_key?(instance_name)
+        # host is in more than one layer - so merge them
+        all_hosts[instance_name][:hostgroups] = all_hosts[instance_name][:hostgroups].merge(node_hostgroups)
+      else
+        all_hosts[instance_name] = {
+          :hostgroups => node_hostgroups,
+          :private_ip => instance_info['private_ip']
+        }
