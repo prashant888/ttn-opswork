4482d434b00cbfdd98e423c6705be2a7f75e2148
prakashul <prakashul@tothenew.com>
2016-03-11 15:01:30 +0530

    nagios server changes

--- nagios/recipes/default.rb
+++ nagios/recipes/default.rb
@@ -6,6 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+nodes = search(:node, "*:*")
+dbnodes.each do |node|
+  Chef::Log.info("#{node["name"]} has IP address #{node["ipaddress"]}")
+end
+
 instance = search("aws_opsworks_instance").each do |instance|
  Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}'
  **********")
