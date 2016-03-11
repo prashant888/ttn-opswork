dac773a0e3da79c89ade2d458e6f8ecdb411606a
prakashul <prakashul@tothenew.com>
2016-03-11 12:21:03 +0530

    testing nagis config

--- nagios/recipes/instance.rb
+++ nagios/recipes/instance.rb
@@ -1,11 +1,3 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 instance = search("aws_opsworks_instance").first
 Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}'
  **********")
