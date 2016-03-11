db0cc1bb6b698b0ad5ae5b98005e023b84ed1ef8
prakashul <prakashul@tothenew.com>
2016-03-11 19:55:59 +0530

    changed opsworks

--- nagios/recipes/db.rb
+++
@@ -1,7 +0,0 @@
-inst_hostname = data_bag('aws_opsworks_instance').each do |hostname|
-  data_bag_item('aws_opsworks_instance', hostname)["source"]
-end
-file '/home/ubuntu/file.txt' do
-  variables(
-    :inst_hostname => inst_hostname
-end
