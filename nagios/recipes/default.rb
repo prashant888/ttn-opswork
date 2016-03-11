029c69f1e9f68bcf0979382e8026b569c732f265
prakashul <prakashul@tothenew.com>
2016-03-11 12:31:24 +0530

    testing nagis config

--- nagios/recipes/default.rb
+++ nagios/recipes/default.rb
@@ -9,11 +9,11 @@
 instance = search("aws_opsworks_instance").first
 Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}'
  **********")
-Chef::Log.info("********** The instance's ID is '#{instance['ec2_instance_id']}'
+Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}'
 **********")
 search("aws_opsworks_instance").each do |instance|
  Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}'
  **********")
- Chef::Log.info("********** The instance's ID is '#{instance['ec2_instance_id']}'
+ Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}'
  **********")
 end
