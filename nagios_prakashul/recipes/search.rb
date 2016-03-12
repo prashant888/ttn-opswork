3132c628b03effcb2e1da33c0f3cb43832e2613d
prakashul <prakashul@tothenew.com>
2016-03-12 19:54:26 +0530

    added pp

--- nagios_prakashul/recipes/search.rb
+++ nagios_prakashul/recipes/search.rb
@@ -5,8 +5,11 @@ all_hostgroups = {}
 all_instances = search(:node, 'role:*')
 all_instances.each do |instance|
   node_hostgroups = {}
-  node_hostgroups.each do |node_hostgroups|
-    puts node_hostgroups
+  node_hostgroups.each do |node|
+    puts node
+end
+	all_instances.each do |num|
+    puts num
 end
   # add a hostgroup for each layer the instance is in
   end
