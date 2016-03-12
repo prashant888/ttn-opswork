a803b3fb1c7f8bd9f57b590bf682ac464002154e
prakashul <prakashul@tothenew.com>
2016-03-12 17:07:04 +0530

    added pp

--- nagios_prakashul/recipes/recipe-search-configure.rb
+++ nagios_prakashul/recipes/recipe-search-configure.rb
@@ -13,7 +13,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 #
-
+require 'pp' 
 all_hosts = {}
 all_hostgroups = {}
 
