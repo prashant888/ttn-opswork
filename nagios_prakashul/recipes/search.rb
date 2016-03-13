2e560075d74c4da33b8f924ce302f11bd8e7dde2
prakashul <prakashul@tothenew.com>
2016-03-13 12:21:34 +0530

    added pp

--- nagios_prakashul/recipes/search.rb
+++ nagios_prakashul/recipes/search.rb
@@ -3,10 +3,10 @@ all_hostgroups = {}
 
 # Gets all (online only) instances in OpsWorks stack
 all_instances = search(:node, 'role:*')
+puts all_instances.inspect
 all_instances.each do |instance|
-  node_hostgroups = {}
+	node_hostgroups = {}
 end
-  puts node_hostgroups.inspect
 	#instance['opsworks']['layers'].each do |layer_id, layer_info|
 #    node_hostgroups[layer_id] = layer_info['name']
 #  end
