2ad710147aa9ef40b17047ea39c97bd7ab51de82
prakashul <prakashul@tothenew.com>
2016-03-13 12:46:31 +0530

    added pp

--- nagios_prakashul/recipes/recipe-configure.rb
+++ nagios_prakashul/recipes/recipe-configure.rb
@@ -15,7 +15,7 @@
 
 all_hosts = {}
 all_hostgroups = {}
-node['opsworks']['layers']
+node['opsworks']['instance']['layers']
 node['opsworks']['layers'].each do |layer_id, layer_info|
   all_hostgroups[layer_id] = layer_info['name']
   layer_info['instances'].each do |instance_name, instance_info|
