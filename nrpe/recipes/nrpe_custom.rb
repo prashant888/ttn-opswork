fc4d07d39d1d4cf020ebfd33885af6a750bbb24f
prakashul <prakashul@tothenew.com>
2016-03-15 19:15:22 +0530

    latest nagios

--- nrpe/recipes/nrpe_custom.rb
+++ nrpe/recipes/nrpe_custom.rb
@@ -1,3 +1,6 @@
 include_recipe "nrpe::default"
-mon_host.replace(["#{node['ip_master']}"])
-export mon_host
+@mon_host = "['127.0.0.1']" 
+
+def parse_set()
+  @mon_host = "["#{node['ip_master']}"]"
+end
