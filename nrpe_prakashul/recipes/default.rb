74bf404b60c8cfa83b4e4b2683a69bbfb8e2fde3
prakashul <prakashul@tothenew.com>
2016-03-16 15:23:07 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -2,5 +2,5 @@ include_recipe 'nrpe::default'
 node.override['nrpe']['server_address'] = '3.3.3.3'
 node.override['nrpe']['allowed_hosts'] = %w(3.3.3.3)
 
-file_names = '/etc/nagios/nrpe.cfg'
+file_name = '/etc/nagios/nrpe.cfg'
 file_name.gsub!('127.0.0.1','3.3.3.3')
