0e840f3140fb7aa325a09f4f12838c50b35e5f5c
prakashul <prakashul@tothenew.com>
2016-03-16 22:04:16 +0530

    metadata

---
+++ prakashul/recipes/aws.rb
@@ -0,0 +1,9 @@
+ruby_block "something" do
+    block do
+        #tricky way to load this Chef::Mixin::ShellOut utilities
+        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
+        command = "aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d"
+        command_out = shell_out(command)
+	puts command_out
+    end
+end
