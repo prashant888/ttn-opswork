8ff1734c328d47820106c895156bddf29da081cb
prakashul <prakashul@tothenew.com>
2016-03-16 15:02:30 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -1,3 +1,25 @@
 include_recipe 'nrpe::default'
 node.override['nrpe']['server_address'] = '3.3.3.3'
 node.override['nrpe']['allowed_hosts'] = %w(3.3.3.3)
+
+bash 'add allowed hosts' do
+  code <<-EOH
+   cat << EOF >> "/etc/nagios/nrpe.cfg"
+
+##########CHEF NRPE#################
+
+pid_file=/var/run/nagios/nrpe.pid
+server_port=5666
+server_address=3.3.3.3
+nrpe_user=nagios
+nrpe_group=nagios
+dont_blame_nrpe=0
+debug=0
+command_timeout=60
+
+allowed_hosts=3.3.3.3
+include_dir=/etc/nagios/nrpe.d
+
+EOF 
+    EOH
+end
