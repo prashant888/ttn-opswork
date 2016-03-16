ac72191b5eb4dfaf84091dc7fa0bb43574675396
prakashul <prakashul@tothenew.com>
2016-03-16 13:01:05 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -1,3 +1,3 @@
 include_recipe 'nrpe::default'
-node.override['nrpe']['allowed_hosts'] = ['3.3.3.3']
+node.override['nrpe']['allowed_hosts'] = ['3.3.3.3', '2.2.2.2']
 node.override['nrpe']['server_address'] = '3.3.3.3'
