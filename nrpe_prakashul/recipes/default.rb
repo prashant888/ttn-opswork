e159d0adfba915bbfa8c5f11286c70eb0ca3e412
prakashul <prakashul@tothenew.com>
2016-03-16 15:25:43 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -2,5 +2,9 @@ include_recipe 'nrpe::default'
 node.override['nrpe']['server_address'] = '3.3.3.3'
 node.override['nrpe']['allowed_hosts'] = %w(3.3.3.3)
 
-file_name = '/etc/nagios/nrpe.cfg'
-file_name.gsub!('127.0.0.1','3.3.3.3')
+data = File.read("/etc/nagios/nrpe.cfg") 
+filtered_data = data.gsub("127.0.0.1", "3.3.3.3") 
+# open the file for writing
+File.open("/etc/nagios/nrpe.cfg", "w") do |f|
+  f.write(filtered_data)
+end
