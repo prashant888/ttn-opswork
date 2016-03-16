bd54f4b2b3a1d010201f751cddf63d2e000e1ed4
prakashul <prakashul@tothenew.com>
2016-03-16 22:11:44 +0530

    metadata

--- prakashul/recipes/aws.rb
+++ prakashul/recipes/aws.rb
@@ -1,9 +1,19 @@
+#ruby_block "something" do
+#    block do
+#        #tricky way to load this Chef::Mixin::ShellOut utilities
+#        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
+#        command = "aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d"
+#        command_out = shell_out(command)
+#	puts command_out
+#    end
+#end
+
+
 ruby_block "something" do
     block do
         #tricky way to load this Chef::Mixin::ShellOut utilities
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
-        command = "aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d"
-        command_out = shell_out(command)
-	puts command_out
+        command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d`
+	puts command
     end
 end
