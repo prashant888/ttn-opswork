9ca91fba44dcd67bba41f3c2f65c57bbf5bfb1c2
prakashul <prakashul@tothenew.com>
2016-03-16 15:54:16 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -2,15 +2,15 @@ include_recipe 'nrpe::default'
 node.override['nrpe']['server_address'] = '3.3.3.3'
 node.override['nrpe']['allowed_hosts'] = %w(3.3.3.3)
 
+file_names = ['/etc/nagios/nrpe.cfg']
 
-def stringReplace(127.0.0.1, 3.3.3.3, /etc/nagios/nrpe.cfg)
-  aFile = File.open(fileName, "r")
-  aString = aFile.read
-  aFile.close
+file_names.each do |file_name|
+  text = File.read(file_name)
+  new_contents = text.gsub(/allowed_hosts=127.0.0.1/, "allowed_hosts=3.3.3.3")
 
-  aString.gsub!(127.0.0.1, 3.3.3.3)
+  # To merely print the contents of the file, use:
+  puts new_contents
 
-  File.open(fileName, "w") { |file| file << aString }
+  # To write changes to the file, use:
+  File.open(file_name, "w") {|file| file.puts new_contents }
 end
-
-stringReplace(*ARGV)
