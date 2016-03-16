dc981dac0cedd53ff8ac0f43d2c374e53f11851f
prakashul <prakashul@tothenew.com>
2016-03-16 20:08:48 +0530

    latest nagios

--- prakashul/recipes/test_services.rb
+++ prakashul/recipes/test_services.rb
@@ -1,8 +1,17 @@
-
+#!/usr/bin/ruby
 
 service_file_path='/etc/nagios3/hostgroup.d/'
 Dir.chdir("#{service_file_path}")
 Dir.glob('*.cfg') do |files|
 files.gsub!('.cfg','')
+
+template '/etc/nagios3/services.d/\files.cfg' do
+  source 'services.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  variables( :files => #{files} )
+end
+
 puts files
 end
