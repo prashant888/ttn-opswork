4da35fa8360a87617ee43d6f283bb54ab13953f2
prakashul <prakashul@tothenew.com>
2016-03-16 21:11:08 +0530

    metadata

---
+++ prakashul/metadata.rb
@@ -0,0 +1,11 @@
+name             'Prakashul'
+maintainer       'TTND'
+<<<<<<< HEAD
+depends 'prakashul'
+=======
+maintainer_email ''
+license          'All rights reserved'
+description      'Installs/Configures nginx and tomcat'
+version          '0.1.0'
+depends	'prakashul'
+>>>>>>> b1f78c6ba3a7cdec0624477dd6cb14e5d5c55575
