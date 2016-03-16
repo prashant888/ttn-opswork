ed0e418569901f725dc37450cb9edcc6b20a3e87
prakashul <prakashul@tothenew.com>
2016-03-16 16:15:17 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -5,7 +5,7 @@ node.override['nrpe']['allowed_hosts'] = %w(3.3.3.3)
 ruby_block "insert_line" do
   block do
     file = Chef::Util::FileEdit.new("/etc/nagios/nrpe.cfg")
-    file.search_file_replace_line("/allowed_hosts=127.0.0.1/", "allowed_hosts=3.3.3.3")
+    file.search_file_replace_line("/127\.0\.0\.1/", "3\.3\.3\.3")
     file.write_file
   end
 end
