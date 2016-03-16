bba0d49b067010f6fa333c83231d723c18af92eb
prakashul <prakashul@tothenew.com>
2016-03-16 22:20:38 +0530

    metadata

--- prakashul/recipes/aws.rb
+++ prakashul/recipes/aws.rb
@@ -15,5 +15,9 @@ ruby_block "something" do
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
         command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d`
 	puts command
+	command.each do |instance|
+	id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{instance}"`
+	puts id
     end
+end
 end
