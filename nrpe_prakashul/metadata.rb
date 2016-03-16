a1a0dd5621face95b3776259fbdc8c464cccbce6
prakashul <prakashul@tothenew.com>
2016-03-16 11:15:13 +0530

    latest nagios

--- nrpe_prakashul/metadata.rb
+++ nrpe_prakashul/metadata.rb
@@ -5,4 +5,4 @@ license           'Apache 2.0'
 description       'Installs and configures Nagios NRPE client'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.5.2'
-depends 	  'nrpe'
+depends 'nrpe'
