8e24bbe89f1d13d7da755d8001500e632b01e451
prakashul <prakashul@tothenew.com>
2016-03-16 11:19:30 +0530

    latest nagios

--- nrpe_prakashul/metadata.rb
+++ nrpe_prakashul/metadata.rb
@@ -1,8 +1,1 @@
-name              'nrpe'
-maintainer        'Tim Smith'
-maintainer_email  'tsmith84@gmail.com'
-license           'Apache 2.0'
-description       'Installs and configures Nagios NRPE client'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.5.2'
 depends 'nrpe'
