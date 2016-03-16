52b0fd42c73b3d78598fb180d1907496541f4cf1
prakashul <prakashul@tothenew.com>
2016-03-17 00:18:33 +0530

    metadata

--- prakashul/recipes/aws.rb
+++ prakashul/recipes/aws.rb
@@ -13,11 +13,14 @@ ruby_block "something" do
     block do
         #tricky way to load this Chef::Mixin::ShellOut utilities
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
-        command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d`
-	puts command
-	command.each do |instance|
-	id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{instance}"`
+	command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d`
+command.split("\n").each do |x|
+	id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{x}"`
+	replacedX = x.sub(':', '-')
+	#ip = `aws ec2 describe-instances --instance-ids "#{id}" --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' --output text`
 	puts id
-    end
+	puts replacedX
 end
+
+    end
 end
