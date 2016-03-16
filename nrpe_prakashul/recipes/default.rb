cda53fc7dfc0078af142d061493da9ec0bfc696b
prakashul <prakashul@tothenew.com>
2016-03-16 16:01:38 +0530

    latest nagios

--- nrpe_prakashul/recipes/default.rb
+++ nrpe_prakashul/recipes/default.rb
@@ -2,15 +2,10 @@ include_recipe 'nrpe::default'
 node.override['nrpe']['server_address'] = '3.3.3.3'
 node.override['nrpe']['allowed_hosts'] = %w(3.3.3.3)
 
-file_names = ['/etc/nagios/nrpe.cfg']
-
-file_names.each do |file_name|
-  text = File.read(file_name)
-  new_contents = text.gsub(/allowed_hosts=127.0.0.1/, "allowed_hosts=3.3.3.3")
-
-  # To merely print the contents of the file, use:
-  puts new_contents
-
-  # To write changes to the file, use:
-  File.open(file_name, "w") {|file| file.puts new_contents }
+ruby_block "insert_line" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/nagios/nrpe.cfg")
+    file.insert_line_if_no_match("/3.3.3.3/", "3.3.3.3")
+    file.write_file
+  end
 end
