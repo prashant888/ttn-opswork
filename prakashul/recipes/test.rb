92cd00cbf33b1722189982d177028d6c96482b01
prakashul <prakashul@tothenew.com>
2016-03-16 21:36:38 +0530

    latest nagios

--- prakashul/recipes/test.rb
+++ prakashul/recipes/test.rb
@@ -1,7 +1,20 @@
 service_file_path='/etc/nagios3/hostgroup.d/'
-Dir.chdir("#{service_file_path}")
-Dir.glob('*.cfg').each do |files|
+Dir.chdir(#{service_file_path})
+Dir.glob('*.cfg') do |files|
 files.gsub!('.cfg','')
 puts files
-puts exit
+
 end
+
+
+
+
+
+#irb(main):001:0> Dir.chdir("/home/ttnd/fen_prashant/ttn-opswork/nagios/recipes/")
+#=> 0
+#irb(main):002:0> Dir.glob('*.rb') do |files|
+#irb(main):003:1*  files.gsub!('.rb','')
+#irb(main):004:1> 
+#irb(main):005:1*  puts files
+#irb(main):006:1> end
+#
