36b2607660dcdf43b63caab0964967f1ab5c4861
Prashant <prashant.sharma@tothenew.com>
2016-03-17 10:18:58 +0530

    Made changes

--- prakashul/recipes/aws.rb
+++ prakashul/recipes/aws.rb
@@ -18,7 +18,7 @@ command.split("\n").each do |instance_tag|
 		instance_id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{instance_tag}"`
 		replacedX = instance_tag.sub(':', '-')
 		intance_ip = `aws ec2 describe-instances --instance-ids "#{instance_id}" --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' --output text`
-		puts id
+		puts instance_id
 		puts replacedX
 		puts intance_ip
 		end
