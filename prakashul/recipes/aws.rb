a27732b6d6035e04d4dc1de9ba4c6a86f28167ea
Prashant <prashant.sharma@tothenew.com>
2016-03-17 10:40:09 +0530

    Made changes

--- prakashul/recipes/aws.rb
+++ prakashul/recipes/aws.rb
@@ -15,14 +15,15 @@ ruby_block "something" do
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
 	command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text --region 'us-east-1' | sort |uniq -d`
 command.split("\n").each do |instance_tag|
-		instance_id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{instance_tag}" --region 'us-east-1'`
 		replacedX = instance_tag.sub(':', '-')
+		instance_id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{instance_tag}" --region 'us-east-1'`
+		instance_id.split("\n").each do |intance_ids|
+		puts intance_ids
 		intance_ip = `aws ec2 describe-instances --instance-ids "#{instance_id}" --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' --output text --region 'us-east-1'`
-		instance_ip.split("\n").each do |intance_ips|
 		#puts instance_id replacedX intance_ip
 		#puts replacedX
 		#puts intance_ip
-		puts intance_ips
+		#puts intance_ips
 		end
 		end
 
