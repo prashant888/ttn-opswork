dee8567dbbb4a9511df3352bd3148f026c0b27a5
Prashant <prashant.sharma@tothenew.com>
2016-03-17 12:56:31 +0530

    Made Changes

--- prakashul/recipes/aws.rb
+++ prakashul/recipes/aws.rb
@@ -7,26 +7,18 @@
 #	puts command_out
 #    end
 #end
-
-
 ruby_block "something" do
     block do
         #tricky way to load this Chef::Mixin::ShellOut utilities
-        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
-	command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d`
-command.split("\n").each do |instance_tag|
-		replacedX = instance_tag.sub(':', '-')
-		instance_id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{instance_tag}"`
-		#instance_id.split("\n").each do |intance_ids|
-		puts intance_id
-			#intance_ip = `aws ec2 describe-instances --instance-ids #{instance_ids} --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' --output text --region 'us-east-1'`
-		#	puts intance_ip
-		#puts instance_id replacedX intance_ip
-		#puts replacedX
-		#puts intance_ip
-		#puts intance_ips
-		#	end
-		end
+        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+        command = `aws ec2 describe-tags --filters 'Name=key,Values=CostCenter' --query 'Tags[*].[Value]' --output text | sort |uniq -d`
+command.split("\n").each do |x|
+        id =`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running "Name=tag:CostCenter,Values=#{x}"`
+        replacedX = x.sub(':', '-')
+        #ip = `aws ec2 describe-instances --instance-ids "#{id}" --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' --output text`
+        puts id
+        puts replacedX
+end
 
     end
 end
