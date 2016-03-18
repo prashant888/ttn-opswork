ac93f934e9fe89ce567a6c95a1c1fc6f16107b44
prakashul <prakashul@tothenew.com>
2016-03-18 12:59:56 +0530

    metadata

--- prakashul/recipes/nagios_setup.rb
+++ prakashul/recipes/nagios_setup.rb
@@ -26,12 +26,26 @@ directory '/etc/nagios3/services.d' do
 end
 
 
-####Include directories in nagios
-Dir.chdir(nagios_path)
-open('nagios.cfg', 'w') { |f|
-  f << "cfg_dir=/etc/nagios3/host.d\n"
-  f << "cfg_dir=/etc/nagios3/hostgroup.d\n"
-  f << "cfg_dir=/etc/nagios3/services.d\n"
-}
+#####Include directories in nagios
+#Dir.chdir(nagios_path)
+#open('nagios.cfg', 'w') { |f|
+#  f << "cfg_dir=/etc/nagios3/host.d\n"
+#  f << "cfg_dir=/etc/nagios3/hostgroup.d\n"
+#  f << "cfg_dir=/etc/nagios3/services.d\n"
+#}
+#
+
+
+ruby_block "insert_line" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/nagios/nrpe.cfg")
+    file.insert_line_if_no_match(cfg_dir=/etc/nagios3/host.d, newline)
+	file.write_file
+    file.insert_line_if_no_match(cfg_dir=/etc/nagios3/hostgroup.d, newline)
+	file.write_file
+    file.insert_line_if_no_match(cfg_dir=/etc/nagios3/services.d, newline)
+	    file.write_file
+  end
+
 
 notifies :restart, 'service[nagios]', :immediately
