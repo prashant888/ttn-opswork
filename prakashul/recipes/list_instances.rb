81100760453605f14f7352a73aea26b465dbe679
prakashul <prakashul@tothenew.com>
2016-03-18 12:28:08 +0530

    metadata

---
+++ prakashul/recipes/list_instances.rb
@@ -0,0 +1,37 @@
+
+nagios_path='/etc/nagios3'
+
+
+inlude_recipe 'nagios::default'
+
+directory '/etc/nagios3/host.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/etc/nagios3/hostgroup.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/etc/nagios3/services.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
+####Include directories in nagios
+Dir.chdir(nagios_path)
+open('nagios.cfg', 'w') { |f|
+  f << "cfg_dir=/etc/nagios3/host.d\n"
+  f << "cfg_dir=/etc/nagios3/hostgroup.d\n"
+  f << "cfg_dir=/etc/nagios3/services.d\n"
+}
+
+notifies :restart, 'service[nagios]', :immediately
