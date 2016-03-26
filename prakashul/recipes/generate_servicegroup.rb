019ea5a53ab5ca8d4d4e4db2eacbf8100a61e325
prakashul <prakashul@tothenew.com>
2016-03-26 23:30:50 +0530

    installing from package

--- prakashul/recipes/generate_servicegroup.rb
+++ prakashul/recipes/generate_servicegroup.rb
@@ -9,9 +9,7 @@ require 'nagios_parser/object/parser'
 services_file_path='/etc/nagios3/services.d/'
 
 # read config file
-items_basic = {}
-items_advanced = {}
-Dir.glob('/etc/nagios3/services.d/*.cfg') do |config_file|
+Dir.glob("#{services_file_path}*.cfg") do |config_file|
 
 	config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
 	_basic = []
@@ -23,23 +21,25 @@ Dir.glob('/etc/nagios3/services.d/*.cfg') do |config_file|
 			_basic.push("#{node['host_name']},#{node['service_description']}")
 		end
 	end
-	items_basic[config_file] = _basic.join " "
-	items_advanced[config_file] = _advanced.join " "
+	items_basic = _basic.join " "
+	items_advanced = _advanced.join " "
+	
+	puts items_basic
+	puts items_advanced
+# end
 
-end
+# Dir.chdir(services_file_path)
+# Dir.glob('*.cfg') do |files|
+	filename = config_file.gsub!('.cfg','')
 
-Dir.chdir(services_file_path)
-Dir.glob('*.cfg') do |files|
-files.gsub!('.cfg','')
-
-template "/etc/nagios3/servicegroup.d/#{files}.cfg" do |file|
-        source "servicegroup.erb"
-        owner "root"
-        group "root"
-        mode "0755"
-        variables( :items_basic_string => items_basic[file],
-		   :items_advanced_string => items_advanced[file] )
-        end
+	template "/etc/nagios3/servicegroup.d/#{filename}.cfg" do |file|
+		source "servicegroup.erb"
+		owner "root"
+		group "root"
+		mode "0755"
+		variables( :items_basic_string => items_basic,
+			   :items_advanced_string => items_advanced)
+		end
 end
 
 
