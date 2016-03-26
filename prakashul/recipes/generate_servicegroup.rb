bceb50874c990c7641e99cf7d27554c05bbd7135
prakashul <prakashul@tothenew.com>
2016-03-26 23:00:53 +0530

    servicegroup

--- prakashul/recipes/generate_servicegroup.rb
+++ prakashul/recipes/generate_servicegroup.rb
@@ -8,35 +8,37 @@ require 'nagios_parser/object/parser'
 
 services_file_path='/etc/nagios3/services.d/'
 
-# read config file,
-# Dir.glob('/etc/nagios3/services.d/*.cfg') do |config_file|
+# read config file
+items_basic = {}
+items_advanced = {}
+Dir.glob('/etc/nagios3/services.d/*.cfg') do |config_file|
 
-config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
-items_basic = []
-items_advanced = []
-config_parsed['service'].each do |node|
-	if node['service_description'].include? 'Disk_Space'
-		items_advanced.push("#{node['host_name']},#{node['service_description']}")
-	else
-		items_basic.push("#{node['host_name']},#{node['service_description']}")
+	config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
+	_basic = []
+	_advanced = []
+	config_parsed['service'].each do |node|
+		if node['service_description'].include? 'Disk_Space'
+			_advanced.push("#{node['host_name']},#{node['service_description']}")
+		else
+			_basic.push("#{node['host_name']},#{node['service_description']}")
+		end
 	end
+	items_basic[config_file] = items_basic.join " "
+	items_advanced[config_file] = items_advanced.join " "
+
 end
-items_basic_string = items_basic.join " "
-items_advanced_string = items_advanced.join " "
-
-
 
 Dir.chdir(service_file_path)
 Dir.glob('*.cfg') do |files|
 files.gsub!('.cfg','')
 
-template "/etc/nagios3/servicegroup.d/#{files}.cfg" do
+template "/etc/nagios3/servicegroup.d/#{files}.cfg" do |file|
         source "servicegroup.erb"
         owner "root"
         group "root"
         mode "0755"
-        variables( :items_basic_string => items_basic_string,
-		   :items_advanced_string => items_advanced_string )
+        variables( :items_basic_string => items_basic[file],
+		   :items_advanced_string => items_advanced[file] )
         end
 end
 
