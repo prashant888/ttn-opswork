23ecd6489c3afe2c6e78363a0b052394c5594004
prakashul <prakashul@tothenew.com>
2016-03-26 21:49:07 +0530

    servicegroup

--- prakashul/recipes/generate_servicegroup.rb
+++ prakashul/recipes/generate_servicegroup.rb
@@ -1,29 +1,32 @@
-#require 'nagios_parser/object/parser'
-require 'pp'
-#chef_gem 'nagios_parser'
 
-gem_package 'nagios_parser' do
- action :install
+np = gem_package 'nagios_parser' do
+  version: "1.3.0"
+  action :nothing
 end
+
+np.runaction(:install)
+
+Gem.clearpaths
+
+require 'nagios_parser/object/parser'
 
 services_file_path='/etc/nagios3/services.d/'
 
 # read config file,
-Dir.glob('/etc/nagios3/services.d/*.cfg') do |config_file|
+# Dir.glob('/etc/nagios3/services.d/*.cfg') do |config_file|
 
-        config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
-
-        items_basic = []
-        items_advanced = []
-        config_parsed['service'].each do |node|
-                if node['service_description'].include? 'Disk_Space'
-                        items_advanced.push("#{node['host_name']},#{node['service_description']}")
-                else
-                        items_basic.push("#{node['host_name']},#{node['service_description']}")
-                end
-        end
-        items_basic_string = items_basic.join " "
-        items_advanced_string = items_advanced.join " "
+config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
+items_basic = []
+items_advanced = []
+config_parsed['service'].each do |node|
+	if node['service_description'].include? 'Disk_Space'
+		items_advanced.push("#{node['host_name']},#{node['service_description']}")
+	else
+		items_basic.push("#{node['host_name']},#{node['service_description']}")
+	end
+end
+items_basic_string = items_basic.join " "
+items_advanced_string = items_advanced.join " "
 
 
 
