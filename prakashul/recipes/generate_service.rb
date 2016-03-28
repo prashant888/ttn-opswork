b5cb20d17d68f27513e8840cee1915aa261b61d4
prakashul <prakashul@tothenew.com>
2016-03-28 15:10:43 +0530

    installing from package

---
+++ prakashul/recipes/generate_service.rb
@@ -0,0 +1,27 @@
+
+
+
+hostgroup_file_path='/etc/nagios3/hostgroup.d/'
+_hgroup = []
+# read config file
+Dir.glob("#{hostgroup_file_path}*.cfg") do |config_file|
+
+        config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
+        config_parsed['hostgroup'].each do |node|
+                        _hgroup.push("#{node['hostgroup_name']}")
+                end
+        end
+        items_hgroup = _hgroup.join " "
+        puts items_hgroup
+
+
+_hgroup.each do |name|
+
+template "/etc/nagios3/services.d/#{name}.cfg" do
+	source "services.erb"
+	owner "root"
+	group "root"
+	mode "0755"
+	variables( :name => name )
+	end
+end
