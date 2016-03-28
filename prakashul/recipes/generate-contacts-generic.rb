74b6ccb8f2620450f173cd96fd11a5626f7063b6
prakashul <prakashul@tothenew.com>
2016-03-28 12:19:42 +0530

    installing from package

--- prakashul/recipes/generate-contacts-generic.rb
+++ prakashul/recipes/generate-contacts-generic.rb
@@ -1,3 +1,4 @@
+
 np = gem_package 'nagios_parser' do
   version "1.3.0"
   action :install
@@ -5,30 +6,48 @@ end
 
 require 'nagios_parser/object/parser'
 
-hgroups_file_path='/etc/nagios3/hostgroup.d/'
-contacts_file_path='/etc/nagios3/contacts.d/'
+hgroup_file_path='/etc/nagios3/hostgroup.d/'
 
-def cgroups (file)
+# read config file
+Dir.glob("#{hgroup_file_path}*.cfg") do |config_file|
 
-        _parsed = NagiosParser::Object::Parser.parse(File.read(file))
-        _items = []
-        _parsed['hostgroup'].each do |node|
-               _items.push node['hostgroup_name']
-        end
-
-        _items.join ","
-
+	config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
+	_contacts = []
+	config_parsed['hostgroup'].each do |node|
+			_contacts.push("#{node['hostgroup_name']}")
+		end
 	end
+	items_contacts = _contacts.join ","
+	
+	puts items_contacts
 
-	contacts = cgroups("#{hgroups_file_path}#{node}.cfg")
-	puts contacts
-contacts.each do |cont|
-#advanced = cgroups("#{hgroups_file_path}#{node}.cfg")
 
-template "/etc/nagios3/contacts.d/#{node}.cfg" do
-source "contactgroup.erb"
-owner "root"
-group "root"
-mode "0755"
-variables( :advanced => advanced,
-           :basic => basic)
+Dir.chdir(hgroup_file_path)
+Dir.glob('*.cfg') do |files|
+	filename = File.basename(files)
+	template "/etc/nagios3/contacts.d/#{filename}.cfg" do 
+		source "contacts-generic.erb"
+		owner "root"
+		group "root"
+		mode "0755"
+		variables( :items_contacts => items_contacts,
+			   :email => email	)
+		end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# write config files
+#/etc/nagios3/servicegroup.d
+
