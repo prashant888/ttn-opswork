76f5241f23ebaef5ba14e18a3279f938b268d572
prakashul <prakashul@tothenew.com>
2016-03-28 14:21:01 +0530

    installing from package

--- prakashul/recipes/generate_contactgroup.rb
+++ prakashul/recipes/generate_contactgroup.rb
@@ -22,15 +22,15 @@ puts _hgroups
 #Dir.chdir(contacts_file_path)
 #Dir.glob('*.cfg') do |files|
 
-#template "/etc/nagios3/contactgroup.d/cg-generic.cfg" do
-#	source "contactgroup-generic.erb"
-#	owner "root"
-#	group "root"
-#	mode "0755"
-#	variables( :_hgroups => _hgroups)
-#	end
-#
-#
+template "/etc/nagios3/contactgroup.d/cg-generic.cfg" do
+	source "contactgroup-generic.erb"
+	owner "root"
+	group "root"
+	mode "0755"
+	variables( :_hgroups => _hgroups)
+	end
+
+
 
 
 
