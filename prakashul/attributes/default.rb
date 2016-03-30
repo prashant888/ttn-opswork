6004743ed41e08e5a8b0175600c6241472cc41ef
prakashul <prakashul@tothenew.com>
2016-03-30 22:44:25 +0530

    changes in generic services

---
+++ prakashul/attributes/default.rb
@@ -0,0 +1,3 @@
+node.default['nagios']['names'] = ['Kids', 'Learning-Teachervision', 'Learning-Shared', 'Learning-Reference', 'Learning-Familyeducation', 'Kids-Poptropica', 'Kids-Funbrain', 'Kids', 'Fen-Sales', 'Fen-GlobalOps']
+
+node.default['nagios']['emails'] = ['prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com','prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com']
--- prakashul/recipes/generate-contacts-generic.rb
+++ prakashul/recipes/generate-contacts-generic.rb
@@ -9,24 +9,30 @@ chef_gem 'nagios_parser'
 
 
 hgroup_file_path='/etc/nagios3/hostgroup.d/'
-email='prakashul@gmail.com'
-# read config file
-Dir.glob("#{hgroup_file_path}*.cfg") do |config_file|
+#email='prakashul@gmail.com'
+## read config file
+#Dir.glob("#{hgroup_file_path}*.cfg") do |config_file|
+#
+#	config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
+#	_contacts = []
+#	config_parsed['hostgroup'].each do |node|
+#			_contacts.push("#{node['hostgroup_name']}")
+#		end
+#	items_contacts = _contacts.join ","
+#	puts items_contacts
+#
 
-	config_parsed = NagiosParser::Object::Parser.parse(File.read(config_file))
-	_contacts = []
-	config_parsed['hostgroup'].each do |node|
-			_contacts.push("#{node['hostgroup_name']}")
-		end
-	items_contacts = _contacts.join ","
-	puts items_contacts
+node.default['nagios']['names'].each do |name|
 
-template "/etc/nagios3/contacts.d/#{items_contacts}.cfg" do 
+	node.default['nagios']['emails'].each do |email|
+
+
+template "/etc/nagios3/contacts.d/#{name}.cfg" do 
 	 source "contacts-generic.erb"
 	 owner "root"
 	 group "root"
 	 mode "0755"
-	 variables( :items_contacts => items_contacts,
+	 variables( :name => name,
                           :email => email      )
 end
 
