bed561e0c961207267b47301ff536e51d6e2babb
prakashul <prakashul@tothenew.com>
2016-03-30 23:15:16 +0530

    changes in generic services

--- prakashul/attributes/default.rb
+++ prakashul/attributes/default.rb
@@ -1,3 +1,3 @@
 node.default['nagios']['names'] = ['Kids', 'Learning-Teachervision', 'Learning-Shared', 'Learning-Reference', 'Learning-Familyeducation', 'Kids-Poptropica', 'Kids-Funbrain', 'Kids', 'Fen-Sales', 'Fen-GlobalOps']
 
-node.default['nagios']['emails'] = ['prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com','prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com', 'prakashul1@gmail.com']
+node.default['nagios']['emails'] = ['prakashul1@gmail.com', 'prakashul2@gmail.com', 'prakashul3@gmail.com', 'prakashul4@gmail.com','prakashul5@gmail.com', 'prakashul6@gmail.com', 'prakashul7gmail.com', 'prakashul8@gmail.com', 'prakashul9@gmail.com']
